4227401b23007736a7e9101f0c84f05dc20e4ee1
joe.jh.wu <joe.jh.wu@deltaww.com>
2019-11-24 18:10:29 +0800

    joelang first commit

---
+++ shell.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from joelang import *
+
+##########################
+#THIS IS THE ENTRY PLACE#
+##########################
+
+while True:
+	text = input('Joelang > ')
+	if text.strip() == "": continue
+	result, error = joelang.run('<stdin>', text)
+
+	if error:
+		print(error.as_string())
+	elif result:
+		if len(result.elements) == 1:
+			print(repr(result.elements[0]))
+		else:
+			print(repr(result))
